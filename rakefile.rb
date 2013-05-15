7aa903ef47557f5a47c76e00fd0ff80ca830e31b
oge <oskar.gewalli@lundalogik.se>
2013-05-15 18:09:03 +0200

    Ilmerge
    Well, it's to big. Maybe a rewrite to use ordinary .net instead of Nest

--- rakefile.rb
+++ rakefile.rb
@@ -25,6 +25,28 @@ namespace :elasticelmah do
     cp Dir.glob("./src/ElasticElmah.Appender/bin/Debug/ElasticElmah.Appender.dll"), output_directory_lib
     
   end
+  def powershell_command(command)
+      system "powershell -NoProfile -ExecutionPolicy unrestricted -Command \"#{command}\""
+      if ! $?.success?
+        puts "Failed: #{command} with #{$?}"
+        raise "failed to execute powershell!"
+      end        
+  end
+
+  task :merge_runner_to_nuspec do |cfg|
+    output_directory_lib = "./nuget/ElasticElmah.Tail/tools/"
+    mkdir_p output_directory_lib
+    command = 'C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe'  
+    primary= './src/ElasticElmah.Tail/bin/Debug/ElasticElmah.Tail.exe'
+    assemblies = primary, './src/ElasticElmah.Tail/bin/Debug/log4net.dll',
+        './src/ElasticElmah.Tail/bin/Debug/Nest.dll',
+        './src/ElasticElmah.Tail/bin/Debug/Newtonsoft.Json.dll'
+    output =File.join(output_directory_lib, 'ElasticElmah.Tail.exe')
+    assembliesstr = assemblies.map do |a| "\"#{a}\"" end.join(" ")
+    cmd = "& '#{command}' /ndebug /v4 /t:exe /out:\"#{output}\" #{assembliesstr}"
+    #puts cmd
+    powershell_command(cmd)
+  end
 
   desc "create the nuget package"
   task :nugetpack => [:core_nugetpack]
@@ -34,6 +56,5 @@ namespace :elasticelmah do
       sh "..\\..\\src\\.nuget\\NuGet.exe pack ElasticElmah.Appender.nuspec"
     end
   end
-
 
 end
