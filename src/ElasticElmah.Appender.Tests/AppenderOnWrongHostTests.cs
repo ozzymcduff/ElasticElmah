08f10d36f6510325db1586447c345f2737a8c301
Mathieu <gewalli@gmail.com>
2013-05-21 07:15:15 +0200

    What happens when it's the wrong host?

---
+++ src/ElasticElmah.Appender.Tests/AppenderOnWrongHostTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using log4net.Core;
+using log4net;
+using System.Reflection;
+namespace ElasticElmah.Appender.Tests
+{
+    [TestFixture]
+    public class AppenderOnWrongHostTests
+    {
+        protected static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private ElasticSearchAppender _appender;
+        [SetUp]
+        public virtual void Init()
+        {
+            var _index = Guid.NewGuid();
+            var conn = "Server=" + Guid.NewGuid() + ";Index=" + _index + ";Port=9200";
+            _appender = new ElasticSearchAppender() { ConnectionString = conn };
+        }
+
+        [TearDown]
+        public virtual void Cleanup()
+        {
+            //_repo.DeleteIndexAsync();
+        }
+
+        [Test]
+        public virtual void Test()
+        {
+            _appender.Async = true;// not needed
+            _appender.AppendAsync(new LoggingEvent(GetType(), _log.Logger.Repository,
+                    new LoggingEventData
+                    {
+                        TimeStamp = DateTime.Now,
+                        Level = Level.Error,
+                        Message = "Message",
+                        Properties = new log4net.Util.PropertiesDictionary().Tap(d =>
+                        {
+                            d["prop"] = "msg";
+                        })
+                    })).Wait();
+        }
+
+    }
+}
--- src/ElasticElmah.Appender.Tests/AppenderTests.cs
+++ src/ElasticElmah.Appender.Tests/AppenderTests.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using log4net.Core;
 using log4net;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 namespace ElasticElmah.Appender.Tests
 {
     [TestFixture]
@@ -36,20 +34,41 @@ namespace ElasticElmah.Appender.Tests
         public virtual void Test() 
         {
             _appender.Async = true;// not needed
-            _appender.AppendAsync(new LoggingEvent(GetType(), _log.Logger.Repository,
-                    new LoggingEventData
-                    {
-                        TimeStamp = DateTime.Now,
-                        Level = Level.Error,
-                        Message = "Message",
-                        Properties = new log4net.Util.PropertiesDictionary().Tap(d =>
-                        {
-                            d["prop"] = "msg";
-                        })
-                    })).Wait();
+            _appender.AppendAsync(new LoggingEvent(GetType(), _log.Logger.Repository, TestData())).Wait();
             _repo.Refresh();
             var paged = _repo.GetPaged(0, 10);
             Assert.That(paged.Total, Is.EqualTo(1));
         }
+
+        [Test]
+        public virtual void Several_logs()
+        {
+            _appender.Async = true;// not needed
+            var tasks = new List<Task>();
+            for (int i = 0; i < 5; i++)
+            {
+                tasks.Add(_appender.AppendAsync(new LoggingEvent(GetType(), _log.Logger.Repository, TestData())));
+            }
+            Task.WaitAll(tasks.ToArray());
+            _repo.Refresh();
+            var paged = _repo.GetPaged(0, 10);
+            Assert.That(paged.Total, Is.EqualTo(5));
+        }
+
+        private static LoggingEventData TestData()
+        {
+            return new LoggingEventData
+            {
+                TimeStamp = DateTime.Now,
+                Level = Level.Error,
+                Message = "Message",
+                Properties = new log4net.Util.PropertiesDictionary().Tap(d =>
+                {
+                    d["prop"] = "msg";
+                })
+            };
+        }
+
     }
+
 }
