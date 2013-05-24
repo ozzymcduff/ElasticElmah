fe3d4206b88cebb96a889bda53544ad9823e317f
Mathieu <gewalli@gmail.com>
2013-05-24 10:31:38 +0200

    ElasticElmah.Tail

--- rakefile.rb
+++ rakefile.rb
@@ -37,35 +37,27 @@ namespace :elasticelmah do
     cp Dir.glob("./src/ElasticElmah.Appender/bin/Debug/ElasticElmah.Appender.dll"), output_directory_lib
     
   end
-  def powershell_command(command)
-      system "powershell -NoProfile -ExecutionPolicy unrestricted -Command \"#{command}\""
-      if ! $?.success?
-        puts "Failed: #{command} with #{$?}"
-        raise "failed to execute powershell!"
-      end        
-  end
 
-  task :merge_runner_to_nuspec do |cfg|
-    output_directory_lib = "./nuget/ElasticElmah.Tail/tools/"
+  task :runners_copy_to_nuspec => [:build] do
+    output_directory_lib = File.join(dir,"nuget/ElasticElmah.Tail/tools/")
     mkdir_p output_directory_lib
-    command = 'C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe'  
-    primary= './src/ElasticElmah.Tail/bin/Debug/ElasticElmah.Tail.exe'
-    assemblies = primary, './src/ElasticElmah.Tail/bin/Debug/log4net.dll',
-        './src/ElasticElmah.Tail/bin/Debug/Nest.dll',
-        './src/ElasticElmah.Tail/bin/Debug/Newtonsoft.Json.dll'
-    output =File.join(output_directory_lib, 'ElasticElmah.Tail.exe')
-    assembliesstr = assemblies.map do |a| "\"#{a}\"" end.join(" ")
-    cmd = "& '#{command}' /ndebug /v4 /t:exe /out:\"#{output}\" #{assembliesstr}"
-    #puts cmd
-    powershell_command(cmd)
+    cp Dir.glob("./src/ElasticElmah.Tail/bin/Debug/ElasticElmah.Tail.exe"), output_directory_lib
+    cp Dir.glob("./src/ElasticElmah.Tail/bin/Debug/log4net.dll"), output_directory_lib
+    
   end
 
   desc "create the nuget package"
-  task :nugetpack => [:core_nugetpack]
+  task :nugetpack => [:core_nugetpack, :runners_nugetpack]
 
   task :core_nugetpack => [:core_copy_to_nuspec] do |nuget|
     cd File.join(dir,"nuget/ElasticElmah.Appender") do
       sh "..\\..\\src\\.nuget\\NuGet.exe pack ElasticElmah.Appender.nuspec"
+    end
+  end
+
+  task :runners_nugetpack => [:runners_copy_to_nuspec] do |nuget|
+    cd File.join(dir,"nuget/ElasticElmah.Tail") do
+      sh "..\\..\\src\\.nuget\\NuGet.exe pack ElasticElmah.Tail.nuspec"
     end
   end
 
